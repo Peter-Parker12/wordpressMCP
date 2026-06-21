fd1238987e9e843d83233198071704e301c8c41e
Peter Tran <megapolis661@gmail.com>
2026-06-21 08:35:01 +0700

    update Oauthen flow

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 4000;
 const WP_URL = process.env.WP_URL;
